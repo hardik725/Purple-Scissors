df32c009ed8c71921295d7ea2ce84fdb92de2c56
Hardik <hardik.pandey.716@gmail.com>
2025-01-10 22:43:21 +0530

    sign up backend fix

--- Backend/Model/User.js
+++ Backend/Model/User.js
@@ -1,40 +1,12 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  Name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  Email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  Password: {
-    type: String,
-    required: true,
-    minlength: 4,
-  },
-  Age: {
-    type: Number,
-    required: true,
-    min: 0, // Ensures age cannot be negative
-  },
-  Place: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  PhoneNumber: {
-    type: String,
-    required: false,
-    match: [/^\d{10}$/, "Please enter a valid phone number"], // Validates a 10-digit phone number
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  Name: { type: String, required: true },
+  Email: { type: String, required: true, unique: true },
+  Password: { type: String, required: true },
+  Age: { type: Number, required: true },
+  Place: { type: String, required: true },
+  PhoneNumber: { type: String, required: true, unique: true },
 });
 
 const User = mongoose.model("User", UserSchema);
